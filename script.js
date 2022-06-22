fa3c441418d4f4888dfa0ba3100284cab1e72d2a
JolaoluwaGrace <jolaoluwagrace@gmail.com>
2022-06-23 03:03:03 +0800

    javascript task

---
+++ script.js
@@ -0,0 +1,10 @@
+ var myName = "Jolaoluwa Grace";
+ var myHeight = "1.62m";
+ var myCountry = "Nigeria";
+
+ document.getElementById("myName").innerHTML = "My Name is " + myName;
+ document.getElementById("myHeight").innerHTML = "My Height is " + myHeight;
+ document.getElementById("myCountry").innerHTML = "I am from " + myCountry;
+
+
+ 
